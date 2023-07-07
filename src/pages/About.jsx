744737b5efdf7c63b72e15fcb6266931a51d13d2
LuisEnrique25 <luisrg2225@gmail.com>
2023-07-06 18:28:44 -0600

    agregue un boton que me lleva a la pagina principal

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 const About = () => {
+  const currentPage = useSelector(store => store.currentPage)
+
   return (
     <section>
       <h1>About Page</h1>
+      <h2 className=' p-2 text-blue-800'>{currentPage}</h2>
       <NavLink to="/">Start</NavLink>
     </section>
   )
